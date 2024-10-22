000c8856767595af4092abeb9966d9857f369003
Naoya Yoshikawa <yonadev.477@gmail.com>
2024-10-22 09:23:20 +0900

    Added smoke test

--- e2e/smoke_test.js
+++ e2e/smoke_test.js
@@ -1,6 +1,10 @@
 SuiteOf('smoke');
 
-Scenario('example.com にアクセスする',  ({ I }) => {
-    I.amOnPage('https://example.com')
-    I.see('Example Domain')
-});
+Scenario('webサイトを開きログインする', ({I}) => {
+    I.amOnPage('https://fastify-webapp-sample-production-901e.up.railway.app/')
+    I.click('ログインする')
+    I.fillField('ユーザー名', 'user1')
+    I.fillField('パスワード', 'super-strong-passphrase')
+    I.click('ログイン')
+    I.see('user1 さん')
+})
